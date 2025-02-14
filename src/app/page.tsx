589951cf2158972afb6041b5e19307d7cf3b42ce
dopoto <dopoto@gmail.com>
2025-02-14 10:15:10 +0200

    ignoreBuildErrors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
-import Link from "next/link";
+
+
+const mockLocations = [
+  {id: 1, name: "Location1"},
+  {id: 2, name: "Location2"},
+  {id: 3, name: "Location3"},
+]
+
+
+function TopNav() {
+  return (
+    <nav className="flex justify-between items-center p-4 bg-gray-800 text-white">
+      <div>Logo</div>
+      <div>Sign in</div>
+    </nav>
+  );
+}
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-linear-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="border-2 p-5">hello</div>
+    <main className="flex min-h-screen flex-col    bg-linear-to-b from-[#2e026d] to-[#15162c] text-white">
+      <TopNav/>
+      <h1>hello</h1>
+      {mockLocations.map(location => <div key={location.id}>{location.name}</div>)}
     </main>
   );
 }
