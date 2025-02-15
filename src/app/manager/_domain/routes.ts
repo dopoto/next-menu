afc0f1c9c4cd12563e06353d831f1dba3e64ffb9
dopoto <dopoto@gmail.com>
2025-02-15 12:23:53 +0200

    wip

--- src/app/manager/_domain/routes.ts
+++ src/app/manager/_domain/routes.ts
@@ -1,9 +1,28 @@
 type Route = {
-    name: string;
-    segment: string;
-    childRoutes?: Route[]
-}
+  name: string;
+  path: string;
+  childRoutes?: Route[];
+};
 
 export const routes: Route[] = [
-    { name: "Locations", segment: "/manager/locations" },    
-]
+  {
+    name: "Manager",
+    path: "/manager",
+    childRoutes: [
+      {
+        name: "Locations",
+        path: "/manager/locations",
+        childRoutes: [
+          { name: "Add location", path: "/manager/locations/add" },
+        ],
+      },
+      {
+        name: "Another route",
+        path: "/manager/another",
+        childRoutes: [
+          { name: "Add another", path: "/manager/another/add" },
+        ],
+      },
+    ],
+  },
+];
