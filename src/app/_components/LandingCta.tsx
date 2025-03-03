2030f960d2e71ded3029ed9f029664f58ae861e8
dopoto <dopoto@gmail.com>
2025-03-03 16:25:54 +0200

    Misc css fixes

--- src/app/_components/LandingCta.tsx
+++ src/app/_components/LandingCta.tsx
@@ -5,17 +5,29 @@ import { Button } from "~/components/ui/button";
 
 export const LandingCta: React.FC = () => {
   return (
-    <div className="bg-foreground">
-      <div className="mx-auto max-w-7xl px-4 py-12 sm:px-6 lg:flex lg:items-center lg:justify-between lg:px-8 lg:py-16">
-        <h2 className="flex flex-col text-3xl font-extrabold tracking-tight text-white md:text-4xl">
-          <PageTitle textColor="text-accent">Ready to dive in?</PageTitle>
-          <PageSubtitle textColor="text-secondary">
+    <div className="relative overflow-hidden bg-gradient-to-r from-gray-950 via-gray-700 to-gray-950">
+      {/* Dot pattern overlay */}
+      <div className="absolute inset-0 opacity-10">
+        <svg className="h-full w-full" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
+          <defs>
+            <pattern id="dots-pattern" x="0" y="7" width="20" height="20" patternUnits="userSpaceOnUse">
+              <circle cx="2" cy="2" r="1.5" fill="white" />
+            </pattern>
+          </defs>
+          <rect width="100%" height="100%" fill="url(#dots-pattern)" />
+        </svg>
+      </div>     
+      {/* Content */}
+      <div className="relative mx-auto max-w-7xl px-4 py-11 sm:px-6 lg:flex lg:items-center lg:justify-between lg:px-8 lg:py-14">
+        <h2 className="flex flex-col text-3xl font-extrabold tracking-tight  md:text-4xl">
+          <PageTitle textColor="text-white">Ready to dive in?</PageTitle>
+          <PageSubtitle textColor="text-white">
             Create a free account now!
           </PageSubtitle>
         </h2>
         <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
           <Link href="/sign-up">
-            <Button variant={"secondary"} size={"lg"}>
+            <Button variant={"outline"} size={"lg"}>
               Get started
             </Button>
           </Link>
