6e6b4182313aeea87cae1c4cb7fd26d549d1272c
dopoto <dopoto@gmail.com>
2025-03-16 07:27:20 +0200

    Remove onboardingComplete from session claims and update layout redirection logic

--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -4,10 +4,6 @@ declare global {
   interface CustomJwtSessionClaims {
     metadata: {
       tier: string,
-
-      // TODO remove. 
-      onboardingComplete: boolean,
-
       orgName: string,
       currentLocationId: string,
       currentLocationName: string
