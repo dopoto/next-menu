cb8b0dea97257b03346026cd14ff665668624a5f
dopoto <dopoto@gmail.com>
2025-03-23 08:56:36 +0200

    eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,69 +1,32 @@
-import { defineConfig } from "eslint/config";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import drizzle from "eslint-plugin-drizzle";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import drizzlePlugin from "eslint-plugin-drizzle";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
+
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
 });
 
-export default defineConfig([{
-    extends: compat.extends(
-        "next/core-web-vitals",
-        "plugin:@typescript-eslint/recommended-type-checked",
-        "plugin:@typescript-eslint/stylistic-type-checked",
-    ),
-
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
     plugins: {
-        "@typescript-eslint": typescriptEslint,
-        drizzle,
+      drizzle: drizzlePlugin
     },
-
-    languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 5,
-        sourceType: "script",
-
-        parserOptions: {
-            project: "./tsconfig.json",
-        },
-    },
-
     rules: {
-        "@typescript-eslint/array-type": "off",
-        "@typescript-eslint/consistent-type-definitions": "off",
-
-        "@typescript-eslint/consistent-type-imports": ["warn", {
-            prefer: "type-imports",
-            fixStyle: "inline-type-imports",
-        }],
-
-        "@typescript-eslint/no-unused-vars": ["warn", {
-            argsIgnorePattern: "^_",
-        }],
-
-        "@typescript-eslint/require-await": "off",
-
-        "@typescript-eslint/no-misused-promises": ["error", {
-            checksVoidReturn: {
-                attributes: false,
-            },
-        }],
-
-        "drizzle/enforce-delete-with-where": ["error", {
-            drizzleObjectName: ["db", "ctx.db"],
-        }],
-
-        "drizzle/enforce-update-with-where": ["error", {
-            drizzleObjectName: ["db", "ctx.db"],
-        }],
+      "drizzle/enforce-delete-with-where": [
+        "error",
+        { drizzleObjectName: ["db", "ctx.db"] },
+      ],
+      "drizzle/enforce-update-with-where": [
+        "error",
+        { drizzleObjectName: ["db", "ctx.db"] },
+      ],
     },
-}]);
+  },
+];
+
+export default eslintConfig;
