2cdd95eea8860e4efa277edc4418199b5039b0ea
dopoto <dopoto@gmail.com>
2025-03-28 11:25:15 +0200

    Change Plan ui fixes

--- src/app/u/plan/change/_components/PlanChanged.tsx
+++ src/app/u/plan/change/_components/PlanChanged.tsx
@@ -9,9 +9,15 @@ import SvgIcon from "~/app/_components/SvgIcons";
 import { ROUTES } from "~/app/_domain/routes";
 
 export function PlanChanged(props: { fromTier: PriceTier; toTier: PriceTier }) {
+  const toTierCardCustomizations = getCurrentPlanCardCustomizations();
+  toTierCardCustomizations.containerStyle += " w-full";
+
   return (
-    <div className="flex flex-col flex-nowrap gap-4">
-      <PriceTierCard tier={props.fromTier} />
+    <div className="flex flex-col flex-nowrap items-center gap-4">
+      <PriceTierCard
+        tier={props.fromTier}
+        cardCustomizations={{ containerStyle: "w-full" }}
+      />
       <SvgIcon
         kind={"arrowDoodle"}
         className={
@@ -20,7 +26,7 @@ export function PlanChanged(props: { fromTier: PriceTier; toTier: PriceTier }) {
       />
       <PriceTierCard
         tier={props.toTier}
-        cardCustomizations={getCurrentPlanCardCustomizations()}
+        cardCustomizations={toTierCardCustomizations}
       />
       <div className="flex w-full flex-col gap-2 pt-4">
         <Link href={ROUTES.my} className="w-full">
