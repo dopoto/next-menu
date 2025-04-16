75f517a1aa2424159786c70baf39b94050aad309
dopoto <dopoto@gmail.com>
2025-04-16 07:57:07 +0300

    ts-jest

--- jest.config.ts
+++ jest.config.ts
@@ -6,12 +6,9 @@ const createJestConfig = nextJest({
   dir: "./",
 });
 
-// Add any custom config to be passed to Jest
 const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   transform: {
     "node_modules/@t3-oss/.+\\.js$": ["ts-jest", {}],
   },
