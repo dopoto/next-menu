f4ec83343a4dc90ecd7ef1a446a456438acab6c6
dopoto <dopoto@gmail.com>
2025-04-16 07:50:30 +0300

    fix tests

--- jest.config.ts
+++ jest.config.ts
@@ -12,6 +12,11 @@ const config: Config = {
   testEnvironment: "jsdom",
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  transform: {
+    "node_modules/@t3-oss/.+\\.js$": ["ts-jest", {}],
+  },
+  transformIgnorePatterns: ["/node_modules/(?!@t3-oss)"],
+  setupFilesAfterEnv: ["<rootDir>/src/test/test-setup.ts"],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
