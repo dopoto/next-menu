1ada74daf6e0692cadb8514a2bc794a562af5e3e
dopoto <dopoto@gmail.com>
2025-05-06 13:20:05 +0300

    wip content tabs

--- src/app/p/[locationSlug]/_components/PublicMenus.tsx
+++ src/app/p/[locationSlug]/_components/PublicMenus.tsx
@@ -1,13 +1,23 @@
+import ContentTabs, { Section } from '~/app/p/[locationSlug]/_components/ContentTabs';
 import { LocationId } from '~/domain/locations';
 import { getPublicMenusByLocation } from '~/server/queries/menus';
 
 export async function PublicMenus(props: { locationId: LocationId }) {
     const menus = await getPublicMenusByLocation(props.locationId);
-    return (
-        <>
-            {menus.map((m) => (
-                <div key={m.id}>{m.name}</div>
-            ))}
-        </>
-    );
+    const sections: Section[] = menus.map((m) => {
+        return {
+            id: m.id.toString(),
+            title: m.name ?? '',
+            content: (
+                <>
+                    <h1>{m.name}</h1>
+                    <div>1</div>
+                    <div>2</div>
+                    <div>3</div>
+                    <div>4</div>
+                </>
+            ),
+        };
+    });
+    return <ContentTabs sections={sections} />;
 }
