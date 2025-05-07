64395dfba45ea81cbfca778d3813f1f6208b14be
dopoto <dopoto@gmail.com>
2025-05-07 09:35:52 +0300

    cpmare flags, featues

--- src/components/ComparePriceTiers.tsx
+++ src/components/ComparePriceTiers.tsx
@@ -1,88 +1,114 @@
-import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableFooter,
-    TableHead,
-    TableHeader,
-    TableRow,
-  } from "~/components/ui/table"
-  
-  const invoices = [
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '~/components/ui/table';
+import { priceTierFeatures } from '~/domain/price-tier-features';
+import { priceTierFlags } from '~/domain/price-tier-flags';
+import { priceTiers } from '~/domain/price-tiers';
+
+const invoices = [
     {
-      invoice: "INV001",
-      paymentStatus: "Paid",
-      totalAmount: "$250.00",
-      paymentMethod: "Credit Card",
+        invoice: 'INV001',
+        paymentStatus: 'Paid',
+        totalAmount: '$250.00',
+        paymentMethod: 'Credit Card',
     },
     {
-      invoice: "INV002",
-      paymentStatus: "Pending",
-      totalAmount: "$150.00",
-      paymentMethod: "PayPal",
+        invoice: 'INV002',
+        paymentStatus: 'Pending',
+        totalAmount: '$150.00',
+        paymentMethod: 'PayPal',
     },
     {
-      invoice: "INV003",
-      paymentStatus: "Unpaid",
-      totalAmount: "$350.00",
-      paymentMethod: "Bank Transfer",
+        invoice: 'INV003',
+        paymentStatus: 'Unpaid',
+        totalAmount: '$350.00',
+        paymentMethod: 'Bank Transfer',
     },
     {
-      invoice: "INV004",
-      paymentStatus: "Paid",
-      totalAmount: "$450.00",
-      paymentMethod: "Credit Card",
+        invoice: 'INV004',
+        paymentStatus: 'Paid',
+        totalAmount: '$450.00',
+        paymentMethod: 'Credit Card',
     },
     {
-      invoice: "INV005",
-      paymentStatus: "Paid",
-      totalAmount: "$550.00",
-      paymentMethod: "PayPal",
+        invoice: 'INV005',
+        paymentStatus: 'Paid',
+        totalAmount: '$550.00',
+        paymentMethod: 'PayPal',
     },
     {
-      invoice: "INV006",
-      paymentStatus: "Pending",
-      totalAmount: "$200.00",
-      paymentMethod: "Bank Transfer",
+        invoice: 'INV006',
+        paymentStatus: 'Pending',
+        totalAmount: '$200.00',
+        paymentMethod: 'Bank Transfer',
     },
     {
-      invoice: "INV007",
-      paymentStatus: "Unpaid",
-      totalAmount: "$300.00",
-      paymentMethod: "Credit Card",
+        invoice: 'INV007',
+        paymentStatus: 'Unpaid',
+        totalAmount: '$300.00',
+        paymentMethod: 'Credit Card',
     },
-  ]
-  
-  export function ComparePriceTiers() {
+];
+
+export function ComparePriceTiers() {
     return (
-      <Table>
-         
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">Invoice</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Method</TableHead>
-            <TableHead className="text-right">Amount</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {invoices.map((invoice) => (
-            <TableRow key={invoice.invoice}>
-              <TableCell className="font-medium">{invoice.invoice}</TableCell>
-              <TableCell>{invoice.paymentStatus}</TableCell>
-              <TableCell>{invoice.paymentMethod}</TableCell>
-              <TableCell className="text-right">{invoice.totalAmount}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-        <TableFooter>
+        <Table>
+            <TableHeader>
+                <TableRow>
+                    <TableHead className="w-[250px]"> </TableHead>
+                    {Object.values(priceTiers)
+                        .filter((tier) => tier.isPublic)
+                        .map((tier) => (
+                            <TableHead key={tier.id} className="text-xl text-center">
+                                {tier.name}
+                            </TableHead>
+                        ))}
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {Object.values(priceTierFeatures).map((feature) => (
+                    <TableRow key={feature.id}>
+                        <TableCell className="font-medium capitalize flex flex-col">
+                          <div>{feature.resourcePluralName}</div>
+                          <div className="font-extralight">{feature.description}</div>
+                          </TableCell>
+                        {Object.values(priceTiers)
+                            .filter((tier) => tier.isPublic)
+                            .map((tier) => {
+                                const featureInTier = tier.features.find((f) => f.id === feature.id);
+                                const quota = featureInTier?.quota ?? 0;
+                                return (
+                                    <TableCell key={tier.id} className="text-center">
+                                        {quota === 0 ? "--" : quota}
+                                    </TableCell>
+                                );
+                            })}
+                    </TableRow>
+                ))}
+                {Object.values(priceTierFlags).map((flag) => (
+                    <TableRow key={flag.id}>
+                        <TableCell className="font-medium capitalize flex flex-col">
+                          <div>{flag.resourcePluralName}</div>
+                          <div className="font-extralight">{flag.description}</div>
+                          </TableCell>
+                        {Object.values(priceTiers)
+                            .filter((tier) => tier.isPublic)
+                            .map((tier) => {
+                                const flagInTier = tier.flags.find((f) => f.id === flag.id);
+                                const isEnabled = flagInTier?.isEnabled ?? false;
+                                return (
+                                    <TableCell key={tier.id} className="text-center">
+                                        {isEnabled ? "yes" : "no"}
+                                    </TableCell>
+                                );
+                            })}
+                    </TableRow>
+                ))}
+            </TableBody>
+            {/*<TableFooter>
           <TableRow>
             <TableCell colSpan={3}>Total</TableCell>
             <TableCell className="text-right">$2,500.00</TableCell>
           </TableRow>
-        </TableFooter>
-      </Table>
-    )
-  }
-  
+        </TableFooter> */}
+        </Table>
+    );
+}
