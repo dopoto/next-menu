0b8132d2461df2214efbcd5577e72b18ca24b432
dopoto <dopoto@gmail.com>
2025-05-07 10:36:10 +0300

    widths

--- src/components/ComparePriceTiers.tsx
+++ src/components/ComparePriceTiers.tsx
@@ -8,83 +8,98 @@ import { priceTiers } from '~/domain/price-tiers';
 
 export function ComparePriceTiers() {
     const priceTiersToShow = Object.values(priceTiers).filter((tier) => tier.isPublic).length;
-    const priceTierColWidth = `w-1/${priceTiersToShow-1}`;
     
     return (
-        <Table>
-            <TableHeader>
-                <TableRow className="hover:bg-transparent">
-                    <TableHead />
-                    {Object.values(priceTiers)
-                        .filter((tier) => tier.isPublic)
-                        .map((tier) => {
-                            return (
-                                <TableHead key={tier.id} className={`pb-8 ${priceTierColWidth}`}>
-                                    <div className="flex flex-col gap-2">
-                                        <PriceTierHeader tier={tier} />
-                                        <GetStartedCta tier={tier.id} variant={'outline'} />
-                                    </div>
-                                </TableHead>
-                            );
-                        })}
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                {Object.values(priceTierFeatures).map((feature) => (
-                    <TableRow key={feature.id}>
-                        <TableCell className="font-medium  w-xs flex flex-col">
-                            <div className="capitalize">{feature.resourcePluralName}</div>
-                            <div className="max-w-xs font-extralight break-words whitespace-normal">
-                                {feature.description}
-                            </div>
-                        </TableCell>
-                        {Object.values(priceTiers)
-                            .filter((tier) => tier.isPublic)
-                            .map((tier) => {
-                                const featureInTier = tier.features.find((f) => f.id === feature.id);
-                                const quota = featureInTier?.quota ?? 0;
-                                return (
-                                    <TableCell key={tier.id} className="text-center">
-                                        {quota === 0 ? <BanIcon className="stroke-gray-300 size-5 mx-auto" /> : quota}
-                                    </TableCell>
-                                );
-                            })}
-                    </TableRow>
-                ))}
-                {Object.values(priceTierFlags).map((flag) => (
-                    <TableRow key={flag.id}>
-                        <TableCell className="font-medium capitalize flex flex-col">
-                            <div>{flag.resourcePluralName}</div>
-                            <div className="font-extralight">{flag.description}</div>
-                        </TableCell>
-                        {Object.values(priceTiers)
-                            .filter((tier) => tier.isPublic)
-                            .map((tier) => {
-                                const flagInTier = tier.flags.find((f) => f.id === flag.id);
-                                const isEnabled = flagInTier?.isEnabled ?? false;
-                                return (
-                                    <TableCell key={tier.id} className="text-center">
-                                        {isEnabled ? (
-                                            <CircleCheckIcon className="stroke-green-600 size-5 mx-auto" />
-                                        ) : (
-                                            <BanIcon className="stroke-gray-300 size-5 mx-auto" />
-                                        )}
-                                    </TableCell>
-                                );
-                            })}
-                    </TableRow>
-                ))}
-                <TableRow className="hover:bg-transparent">
-                    <TableHead />
-                    {Object.values(priceTiers)
-                        .filter((tier) => tier.isPublic)
-                        .map((tier) => (
-                            <TableHead key={tier.id} className="pt-8 ">
-                                <GetStartedCta tier={tier.id} variant={'outline'} />
-                            </TableHead>
-                        ))}
-                </TableRow>
-            </TableBody>
-        </Table>
+        <div className="overflow-x-auto">
+            <div className="inline-block min-w-full align-middle">
+                <div className="overflow-hidden">
+                    <table className="min-w-full divide-y divide-gray-200">
+                        <thead>
+                            <tr className="grid" style={{ gridTemplateColumns: `300px repeat(${priceTiersToShow}, 1fr)` }}>
+                                <th className="px-6 py-3 text-left" />
+                                {Object.values(priceTiers)
+                                    .filter((tier) => tier.isPublic)
+                                    .map((tier) => (
+                                        <th key={tier.id} className="px-6 py-3 text-center">
+                                            <div className="flex flex-col items-center gap-2">
+                                                <PriceTierHeader tier={tier} />
+                                                <GetStartedCta tier={tier.id} variant={'outline'} />
+                                            </div>
+                                        </th>
+                                    ))}
+                            </tr>
+                        </thead>
+                        <tbody className="divide-y divide-gray-200">
+                            {Object.values(priceTierFeatures).map((feature) => (
+                                <tr key={feature.id} className="grid" style={{ gridTemplateColumns: `300px repeat(${priceTiersToShow}, 1fr)` }}>
+                                    <td className="px-6 py-4">
+                                        <div className="flex flex-col">
+                                            <div className="font-medium capitalize">{feature.resourcePluralName}</div>
+                                            <div className="text-sm text-gray-500">
+                                                {feature.description}
+                                            </div>
+                                        </div>
+                                    </td>
+                                    {Object.values(priceTiers)
+                                        .filter((tier) => tier.isPublic)
+                                        .map((tier) => {
+                                            const featureInTier = tier.features.find((f) => f.id === feature.id);
+                                            const quota = featureInTier?.quota ?? 0;
+                                            return (
+                                                <td key={tier.id} className="px-6 py-4">
+                                                    <div className="flex justify-center items-center h-full">
+                                                        {quota === 0 ? <BanIcon className="stroke-gray-300 size-5" /> : quota}
+                                                    </div>
+                                                </td>
+                                            );
+                                        })}
+                                </tr>
+                            ))}
+                            {Object.values(priceTierFlags).map((flag) => (
+                                <tr key={flag.id} className="grid" style={{ gridTemplateColumns: `300px repeat(${priceTiersToShow}, 1fr)` }}>
+                                    <td className="px-6 py-4">
+                                        <div className="flex flex-col">
+                                            <div className="font-medium capitalize">{flag.resourcePluralName}</div>
+                                            <div className="text-sm text-gray-500">
+                                                {flag.description}
+                                            </div>
+                                        </div>
+                                    </td>
+                                    {Object.values(priceTiers)
+                                        .filter((tier) => tier.isPublic)
+                                        .map((tier) => {
+                                            const flagInTier = tier.flags.find((f) => f.id === flag.id);
+                                            const isEnabled = flagInTier?.isEnabled ?? false;
+                                            return (
+                                                <td key={tier.id} className="px-6 py-4">
+                                                    <div className="flex justify-center items-center h-full">
+                                                        {isEnabled ? (
+                                                            <CircleCheckIcon className="stroke-green-600 size-5" />
+                                                        ) : (
+                                                            <BanIcon className="stroke-gray-300 size-5" />
+                                                        )}
+                                                    </div>
+                                                </td>
+                                            );
+                                        })}
+                                </tr>
+                            ))}
+                            <tr className="grid" style={{ gridTemplateColumns: `300px repeat(${priceTiersToShow}, 1fr)` }}>
+                                <td className="px-6 py-4" />
+                                {Object.values(priceTiers)
+                                    .filter((tier) => tier.isPublic)
+                                    .map((tier) => (
+                                        <td key={tier.id} className="px-6 py-4">
+                                            <div className="flex justify-center">
+                                                <GetStartedCta tier={tier.id} variant={'outline'} />
+                                            </div>
+                                        </td>
+                                    ))}
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     );
 }
