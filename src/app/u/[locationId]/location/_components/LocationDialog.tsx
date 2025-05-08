d556ce17093d456461790caf2cc0114b9b99349d
dopoto <dopoto@gmail.com>
2025-05-08 13:39:24 +0300

    loca det

--- src/app/u/[locationId]/location/_components/LocationDialog.tsx
+++ src/app/u/[locationId]/location/_components/LocationDialog.tsx
@@ -2,9 +2,9 @@
 
 import { useRouter } from 'next/navigation';
 import { type ReactNode } from 'react';
-import { Dialog, DialogContent } from '~/components/ui/dialog';
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from '~/components/ui/dialog';
 
-export function LocationDialog(props: { children: ReactNode }) {
+export function LocationDialog(props: { locationName: string; children: ReactNode }) {
     const router = useRouter();
 
     const handleOpenChange = (open: boolean) => {
@@ -14,14 +14,28 @@ export function LocationDialog(props: { children: ReactNode }) {
         }
     };
 
+     
+
     return (
         <Dialog open={true} onOpenChange={handleOpenChange}>
-            <DialogContent>
-                {/* <DialogHeader>
-          <DialogTitle>Location</DialogTitle>
-          <DialogDescription> </DialogDescription>
-        </DialogHeader> */}
-                {props.children}
+            <DialogContent className="!max-w-[95vw] !w-[95vw] !h-[95vh] !max-h-[95vh] !pt-4 flex flex-col">
+                <DialogHeader>
+                    <DialogTitle>
+                        <div className="flex w-full gap-2">
+                            <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
+                                {props.locationName.slice(0, 3).toLocaleUpperCase()}
+                            </div>
+                            <div className="grid flex-1 text-left text-sm leading-tight">
+                                <span className="text-tiny truncate antialiased">LOCATION</span>
+                                <span className="truncate font-semibold">{props.locationName}</span>
+                            </div>
+                        </div>
+                    </DialogTitle>
+                    <DialogDescription> </DialogDescription>
+                </DialogHeader>
+                <div className="flex-1 overflow-auto">
+                    {props.children}
+                </div>
             </DialogContent>
         </Dialog>
     );
--- src/components/LocationDetails.tsx
+++ src/components/LocationDetails.tsx
@@ -27,19 +27,9 @@ export async function LocationDetails(props: { id: LocationId }) {
     }
 
     const locationUrl = `${env.NEXT_PUBLIC_APP_URL}${ROUTES.publicLocation(locationData.slug)}`;
-    const locationName = locationData.name ?? '';
 
     return (
         <div className="flex w-full flex-col flex-nowrap gap-4">
-            <div className="flex w-full gap-2">
-                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
-                    {locationName.slice(0, 3).toLocaleUpperCase()}
-                </div>
-                <div className="grid flex-1 text-left text-sm leading-tight">
-                    <span className="text-tiny truncate antialiased">LOCATION</span>
-                    <span className="truncate font-semibold">{locationName}</span>
-                </div>
-            </div>
             <p className="text-center text-sm">Your location is live here:</p>
             <div className="flex w-full flex-row flex-nowrap gap-2 rounded">
                 <a title={locationUrl} href={locationUrl} target="_blank">
