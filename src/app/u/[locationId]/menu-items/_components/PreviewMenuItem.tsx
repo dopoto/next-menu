fa87e09b9bacf250e40868f0555222b6529a1ffc
dopoto <dopoto@gmail.com>
2025-05-11 11:05:07 +0300

    force-light

--- src/app/u/[locationId]/menu-items/_components/PreviewMenuItem.tsx
+++ src/app/u/[locationId]/menu-items/_components/PreviewMenuItem.tsx
@@ -6,20 +6,18 @@ import { type MenuItem } from '~/domain/menu-items';
 export function PreviewMenuItem(props: { menuItem: Partial<MenuItem>; currencyId: CurrencyId }) {
     const { name, description, price, isNew } = props.menuItem;
     return (
-        <>
-            <DeviceMockup>
-                <div className="flex h-full w-full items-center justify-center rounded-[2rem] bg-gray-100 dark:bg-gray-800">
-                    <PublicMenuItem
-                        item={{
-                            name,
-                            description,
-                            price,
-                            isNew,
-                        }}
-                        currencyId={props.currencyId}
-                    />
-                </div>
-            </DeviceMockup>
-        </>
+        <DeviceMockup>
+            <div className="flex h-full w-full items-center justify-center rounded-[2rem]   ">
+                <PublicMenuItem
+                    item={{
+                        name,
+                        description,
+                        price,
+                        isNew,
+                    }}
+                    currencyId={props.currencyId}
+                />
+            </div>
+        </DeviceMockup>
     );
 }
