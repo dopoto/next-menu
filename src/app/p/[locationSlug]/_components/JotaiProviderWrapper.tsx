d63e11c506ffccad4c6162f9003924b9888582a9
dopoto <dopoto@gmail.com>
2025-05-15 17:36:52 +0300

    jotai

--- src/app/p/[locationSlug]/_components/JotaiProviderWrapper.tsx
+++ src/app/p/[locationSlug]/_components/JotaiProviderWrapper.tsx
@@ -7,6 +7,16 @@ import { orderAtom } from '~/app/p/[locationSlug]/_state/cart';
 import { CurrencyId } from '~/domain/currencies';
 import { LocationId } from '~/domain/locations';
 
+import type { DevToolsProps } from 'jotai-devtools';
+import dynamic from 'next/dynamic';
+import type { ComponentType } from 'react';
+
+let DevTools: ComponentType<DevToolsProps> | null = null;
+
+if (process.env.NODE_ENV !== 'production') {
+    DevTools = dynamic(() => import('./JotaiDevTools').then((mod) => ({ default: mod.DevTools })), { ssr: false });
+}
+
 function Initializer(props: { locationId: LocationId; currencyId: CurrencyId }) {
     const setOrder = useSetAtom(orderAtom);
 
@@ -25,6 +35,7 @@ export default function JotaiProviderWrapper(props: {
     return (
         <Provider>
             <Initializer locationId={props.locationId} currencyId={props.currencyId} />
+            {DevTools && <DevTools />}
             {props.children}
         </Provider>
     );
